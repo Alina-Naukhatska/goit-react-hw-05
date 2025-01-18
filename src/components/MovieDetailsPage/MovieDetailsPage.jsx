6285cb7e8e9a9e028283430bd8b1bb6e410e2d7c
Alina Naukhatska <naukhatska.alina@gmail.com>
2025-01-18 13:06:09 +0200

    finally

--- src/components/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,11 +1,14 @@
-import { useParams, Link, Outlet, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useParams, Link, Outlet, useNavigate, useLocation } from 'react-router-dom';
+import { useEffect, useState, useRef } from 'react';
 import { fetchMovieDetails } from '../../Services/moviesAPI';
 import s from './MovieDetailsPage.module.css';
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const location = useLocation(); 
+  const goBackLink = useRef(location.state?.from || '/'); 
+
   const [movie, setMovie] = useState(null);
 
   useEffect(() => {
@@ -23,24 +26,36 @@ const MovieDetailsPage = () => {
   if (!movie) return <p>Loading...</p>;
 
   const bannerUrl = movie.backdrop_path 
-    ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
+    ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}` 
     : null;
-  
+
   return (
     <div className={s.details}>
-      <button onClick={handleGoBack}>Go back</button>
-      {bannerUrl && <img src={bannerUrl} alt={`${movie.title} banner`} className={s.banner} />}
-      <h1>
-        {movie.title} ({movie.release_date?.slice(0, 4)})
-      </h1>
-      <p>User Score: {movie.vote_average * 10}%</p>
-      <p>Overview: {movie.overview}</p>
-      <p>Genres: {movie.genres.map((genre) => genre.name).join(', ')}</p>
-      <div>
-        <h2>Additional Information</h2>
-        <Link to="cast">Cast</Link>
-        <Link to="reviews">Reviews</Link>
+      <Link to={goBackLink.current}>Go back</Link> 
+      <div className={s.content}>
+        {bannerUrl && <img src={bannerUrl} alt={`${movie.title} banner`} className={s.banner} />}
+        <div className={s.text}>
+          <h1>{movie.title} ({movie.release_date?.slice(0, 4)})</h1>
+          <p>User Score: {movie.vote_average * 10}%</p>
+          <h2>Overview: </h2>
+          <p> {movie.overview}</p>
+          <h2>Genres: </h2>
+          <p>{movie.genres.map((genre) => genre.name).join(', ')}</p>
+        </div>
       </div>
+      <div className={s.separator}></div>
+      <div>
+  <h2>Additional Information</h2>
+  <ul className={s.linkList}>
+    <li>
+      <Link to={`/movies/${movieId}/cast`} className={s.link}>Cast</Link>
+    </li>
+    <li>
+      <Link to={`/movies/${movieId}/reviews`} className={s.link}>Reviews</Link>
+    </li>
+  </ul>
+</div>
+      <div className={s.separator}></div>
       <Outlet />
     </div>
   );
